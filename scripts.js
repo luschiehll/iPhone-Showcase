13cfe07ee0eed8673d267b3f1dff67eb927cddfc
luschiehll <schiehlllucas73@gmail.com>
2023-04-05 19:41:24 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function trocaCor(cor) {
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+
+function trocaImagem(img) {
+    let imgIphone = document.querySelector(".iphone")
+    imgIphone.src= img
+}
